675c624d06fdfe38be1b5d7cbd5790735795b2f4
jessikabunga <149929654+jessikabunga@users.noreply.github.com>
2024-05-19 18:38:35 +0700

    Delete deskripsi/kulkas sharp directory

--- deskripsi/kulkas sharp/deskripsi.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const bookmarkIcon = document.getElementById('bookmark');
-    bookmarkIcon.addEventListener('click', () => {
-        bookmarkIcon.classList.toggle('clicked');
-    });
-});
